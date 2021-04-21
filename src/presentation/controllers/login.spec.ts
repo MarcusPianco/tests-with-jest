5974efba5c669362982180611e7dbfa3fadf0c3d
Marcus Piancó <marcusjrdev@gmail.com>
2021-04-21 19:53:10 -0300

    test: check if email is valid in LoginController

--- src/presentation/controllers/login.spec.ts
+++ src/presentation/controllers/login.spec.ts
@@ -1,10 +1,22 @@
+import { InvalidParamsError } from '../errors/invalid-params-error'
 import { MissingParamsError } from '../errors/missing-params-error'
+import { EmailValidator } from '../protocols/email-validator'
 import { HttpRequest } from '../protocols/http'
 import { LoginController } from './login'
 
+const makeEmailValidatorStub = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  return new EmailValidatorStub()
+}
+
 describe('LoginController', () => {
   test('should returns badRequest when email not provide',async () => {
-    const loginControllerSut = new LoginController()
+    const emailValidatorStub = makeEmailValidatorStub()
+    const loginControllerSut = new LoginController(emailValidatorStub)
 
     const httpRequest: HttpRequest = {
       body: {
@@ -17,7 +29,8 @@ describe('LoginController', () => {
   })
 
   test('should returns badRequest when password not ptovide',async () => {
-    const loginControllerSut = new LoginController()
+    const emailValidatorStub = makeEmailValidatorStub()
+    const loginControllerSut = new LoginController(emailValidatorStub)
 
     const httpRequest: HttpRequest = {
       body: {
@@ -27,5 +40,21 @@ describe('LoginController', () => {
     const httpResponse = await loginControllerSut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamsError('password'))
+  })
+  test('should returns badRequest when email is invalid',async () => {
+    const emailValidatorStub = makeEmailValidatorStub()
+    const loginControllerSut = new LoginController(emailValidatorStub)
+
+    const httpRequest: HttpRequest = {
+      body: {
+        email: 'invalid_email',
+        password: 'valid_password'
+      }
+    }
+    jest.spyOn(emailValidatorStub,'isValid').mockReturnValueOnce(false)
+    const httpResponse = await loginControllerSut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamsError('email'))
   })
 })
