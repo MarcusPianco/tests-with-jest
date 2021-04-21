701ca11b03a14b57f05cb022f003ecf9c3b74033
Marcus Piancó <marcusjrdev@gmail.com>
2021-04-21 19:18:29 -0300

    test: ensure email missing validation in LoginController

--- src/presentation/controllers/login.spec.ts
+++ src/presentation/controllers/login.spec.ts
@@ -1,5 +1,16 @@
-describe('testing husky config', () => {
-  test('',() => {
-    expect(1).toEqual(1)
+import { MissingParamsError } from '../errors/missing-params-error'
+import { HttpRequest } from '../protocols/http'
+import { LoginController } from './login'
+
+describe('LoginController', () => {
+  test('should returns badRequest when email not passed',async () => {
+    const loginControllerSut = new LoginController()
+
+    const httpRequest: HttpRequest = {
+      body: {}
+    }
+    const httpResponse = await loginControllerSut.handle(httpRequest)
+
+    expect(httpResponse.body).toEqual(new MissingParamsError('email'))
   })
 })
