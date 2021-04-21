6515aff5962fe2728f49af6b65aaff4df5aa7466
Marcus Piancó <marcusjrdev@gmail.com>
2021-04-21 14:05:54 -0300

    chore: configure jest env

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1'
+  },
+  coveragePathIgnorePatterns: [
+    '<rootDir>/src/presentation/controllers/signup/signup-protocols.ts',
+    '<rootDir>/src/presentation/protocols'
+  ]
+}
---
+++ src/presentation/controllers/login.spec.ts
@@ -0,0 +1,5 @@
+describe('Name of the group', () => {
+  test('',() => {
+    expect(1).toEqual(1)
+  })
+})
