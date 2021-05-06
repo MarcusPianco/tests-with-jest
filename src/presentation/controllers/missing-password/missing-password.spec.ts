76cc114a4b4bfbc6a01cf25698ef65de1daa9ff9
Marcus Piancó <marcusjrdev@gmail.com>
2021-05-05 23:20:21 -0300

    refactor: create tests factories

--- src/presentation/controllers/missing-password/missing-password.spec.ts
+++ src/presentation/controllers/missing-password/missing-password.spec.ts
@@ -2,6 +2,11 @@ import { UserModel } from '@/domain/models/user'
 import { FindUser } from '@/domain/usecases/find-user'
 import { MissingPasswordController } from './missing-password'
 
+interface SutTypes{
+  sut: MissingPasswordController
+  findUser: FindUser
+}
+
 const makeFindUserStub = (): FindUser => {
   class FindUserStub implements FindUser {
     async find (email: string): Promise<UserModel> {
@@ -16,18 +21,26 @@ const makeFindUserStub = (): FindUser => {
   return new FindUserStub()
 }
 
+const makeSut = (): SutTypes => {
+  const findUser = makeFindUserStub()
+  const sut = new MissingPasswordController(findUser)
+  return {
+    sut,
+    findUser
+  }
+}
+
 describe('MissingPasswordController', () => {
   test('should called find user with correct data',async () => {
-    const findUserUseCase = makeFindUserStub()
-    const missingPasswordController = new MissingPasswordController(findUserUseCase)
-    const spyFind = jest.spyOn(findUserUseCase,'find')
+    const { sut, findUser } = makeSut()
+    const spyFind = jest.spyOn(findUser,'find')
 
     const httpRequest = {
       body: {
         email: 'valid_email@email.com'
       }
     }
-    await missingPasswordController.handle(httpRequest)
+    await sut.handle(httpRequest)
     expect(spyFind).toBeCalledWith('valid_email@email.com')
   })
 })
