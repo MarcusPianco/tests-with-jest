45d21412311bfcdd960e5889e7ca96255a593158
Marcus Piancó <marcusjrdev@gmail.com>
2021-05-12 23:08:38 -0300

    feat: ensure email validation in MissingPasswordController

--- src/presentation/controllers/missing-password/missing-password.ts
+++ src/presentation/controllers/missing-password/missing-password.ts
@@ -1,16 +1,27 @@
 import { FindUser } from '@/domain/usecases/find-user'
+import { InvalidParamsError } from '@/presentation/errors/invalid-params-error'
+import { badRequest } from '@/presentation/helpers/http-helper'
 import { Controller } from '@/presentation/protocols/controller'
+import { EmailValidator } from '@/presentation/protocols/email-validator'
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
 
 export class MissingPasswordController implements Controller {
   private readonly _findUser: FindUser
+  private readonly _emailValidator: EmailValidator
 
-  constructor (findUser: FindUser) {
+  constructor (findUser: FindUser, emailValidator: EmailValidator) {
     this._findUser = findUser
+    this._emailValidator = emailValidator
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this._findUser.find(httpRequest.body.email)
+    const { email } = httpRequest.body
+
+    if (!this._emailValidator.isValid(email)) {
+      return badRequest(new InvalidParamsError('email'))
+    }
+
+    await this._findUser.find(email)
 
     return Promise.resolve({ statusCode: 200, body: 'email recovery was send' })
   }
